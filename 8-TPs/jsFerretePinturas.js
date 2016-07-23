53f7af5ba4b88feb57f0947ee45573dbccce342f
Proyectos UTN <carla.e.vargas@gmail.com>
2016-07-22 23:08:30 -0300

    Signed-off-by: Proyectos UTN <carla.e.vargas@gmail.com>

--- 8-TPs/jsFerretePinturas.js
+++ 8-TPs/jsFerretePinturas.js
@@ -4,10 +4,26 @@ B.	Al ingresar una temperatura en Centígrados debemos mostrar la temperatura en
 */
 function FahrenheitCentigrados () 
 {
-	
+
+	//var temp;
+
+	var temp = document.getElementById('Temperatura').value;
+	 
+	 var fahrenACent = (parseInt(temp) - 32)/1.8;
+
+	 alert( "Centígrados a Fahrenheit: "+ fahrenACent);
+
+	 //f = c*1.8+32
 }
 
 function CentigradosFahrenheit () 
 {
+	
+
+	 var temp = document.getElementById('Temperatura').value;
+	 
+	 var centAFarenh = parseInt(temp) * 1.8 + 32;
+
+	 alert( "Centígrados a Fahrenheit: " +centAFarenh);
 	
 }
